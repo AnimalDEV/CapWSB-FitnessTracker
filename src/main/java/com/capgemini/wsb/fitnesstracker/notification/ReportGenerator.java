b388e2b9594d56f06d7bb3c0cb817548165b3185
Maciej Lasoń <maciejlason3@gmail.com>
2024-05-28 22:57:31 +0200

    fix: fixed tests

--- src/main/java/com/capgemini/wsb/fitnesstracker/notification/ReportGenerator.java
+++ src/main/java/com/capgemini/wsb/fitnesstracker/notification/ReportGenerator.java
@@ -7,6 +7,7 @@ import com.capgemini.wsb.fitnesstracker.training.api.TrainingService;
 import com.capgemini.wsb.fitnesstracker.training.internal.TrainingServiceImpl;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Profile;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
